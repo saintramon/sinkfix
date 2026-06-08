50877a741a12d9ae05b96e52b16caea6cb290033
saintramon <ramonjasmin8@gmail.com>
2026-06-08 23:39:21 +0800

    feat: added handleSubmit priv func

--- frontend/src/features/analysis/components/AnalysisForm.tsx
+++ frontend/src/features/analysis/components/AnalysisForm.tsx
@@ -10,4 +10,28 @@ export function AnalysisForm({ isSubmitting, onSubmit }: AnalysisFormProps) {
     const[modelName, setModelName] = useState("google-bert/bert-base-uncased");
     const[text, setText] = useState("");
     const[error, setError] = useState<string | null>(null);
+
+    function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        let trimmedModelname = modelName.trim();
+        let trimmedText = text.trim();
+
+        if(!trimmedModelname) {
+            setError("Model name is required");
+            return;
+        }
+
+        if(!trimmedText) {
+            setError("Text is required")
+        }
+
+        setError(null);
+
+        onSubmit({
+            modelName: trimmedModelname,
+            text: trimmedText,
+        });
+    }
+    
 }
